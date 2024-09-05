2d326ce21c20287dac71846b3c04a932333ff854
andrewhenrique2 <andrewloly06@gmail.com>
2024-09-05 18:22:15 -0300

    fix: small syntax error in tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,8 +15,9 @@ const config: Config = {
         "custom-curve": "polygon(0 0, 100% 0, 100% 85%, 0 100%)",
       },
       fontFamily: {
-        sans: ['Plus Jakarta Sans"', "sans-serif"],
+        sans: ['"Plus Jakarta Sans"', "sans-serif"], 
       },
+      
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
